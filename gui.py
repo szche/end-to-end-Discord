0d6f7533ca50cf2e1ed0fd53a078443f78c3e835
unknown <github@chadam.pl>
2021-02-09 11:57:03 +0100

    major GUI improvements

--- gui.py
+++ gui.py
@@ -2,6 +2,8 @@ import tkinter as tk
 import tkinter.font as tkFont
 import tkinter.ttk as ttk
 
+DC_TOKEN = "" 
+
 class GUI(object):
     def __init__(self):
         self.root = tk.Tk()
@@ -27,27 +29,39 @@ class GUI(object):
 
         loginLabel = tk.Label(self.loginPage, text="Input your Discord token" ,bg="white", \
                                 width=45, height=2, anchor="w", pady=0)
-        loginLabel['font'] = tkFont.Font(family = "Arial Narrow", size=40)
+        loginLabel['font'] = tkFont.Font(size=40)
         loginLabel.grid(row=1, column=0, padx=70, pady=0)
 
-        self.tokenEntry = tk.Entry(self.loginPage, width=30, bd=2, \
+        self.tokenEntry = tk.Entry(self.loginPage, width=70, bd=2, \
                                         relief="flat", highlightbackground="#b9c4f1", \
                                         highlightthickness=2)
+        self.tokenEntry.insert(0, DC_TOKEN)
         self.tokenEntry.config(highlightbackground="black", highlightcolor="black", border=4)
-        self.tokenEntry['font'] = tkFont.Font(family="Arial", size=35)
+        self.tokenEntry['font'] = tkFont.Font(size=15)
         self.tokenEntry.grid(row=2, column=0, sticky="w", padx=70, pady=0)
 
-        buttonStartingPage = tk.Button(self.loginPage, bg="black",anchor="center", fg="white",bd=0, \
+        self.option = tk.IntVar(value=1)
+        remember_checkbox = tk.Checkbutton(self.loginPage, text='Remember me', bg="white", relief="flat", variable=self.option, onvalue=1, offvalue=0)
+        remember_checkbox['font'] = tkFont.Font(size=15)
+        remember_checkbox.grid(row=3, column=0, sticky="w", padx=70, pady=20)
+
+        loginButton = tk.Button(self.loginPage, bg="black",anchor="center", fg="white",bd=0, \
                                         height=2, activebackground="white", text="Login", \
                                         width=10, command=self.collect_dcToken)
-        buttonStartingPage['font'] = tkFont.Font(family="Arial", size=12, weight="bold")
-        buttonStartingPage.grid(row=3, column=0, pady=50, padx=70, sticky = "w")
+        loginButton['font'] = tkFont.Font(size=12, weight="bold")
+        loginButton.grid(row=4, column=0, pady=20, padx=70, sticky = "w")
 
     def collect_dcToken(self):
         token = self.tokenEntry.get()
+        remember = self.option.get()
+
         print(f"Collected token: {token}")
+
+        if remember == 1:
+            with open("dcToken.txt", "w+") as f:
+                f.write(token)
+
         #TODO login with the discord bot
-        self.configure_mainPage()
         self.showPage(self.mainPage)
 
     def configure_mainPage(self):
@@ -66,29 +80,25 @@ class GUI(object):
         chatHistory['font'] = tkFont.Font(family="Arial", size=20)
         chatHistory.place(x=50, y=40)
 
-        # pole 
         self.chatEntry = tk.Entry(left_frame, bd=2, bg="white", width=26, relief="flat", selectborderwidth=2, highlightthickness=2)
         self.chatEntry.config(highlightbackground="black", highlightcolor="black")
         self.chatEntry['font'] = tkFont.Font(family="Arial", size=26)
         self.chatEntry.place(x=50, y=640)
 
-        #przycik wyslij w narzedzia
-        PrzyciskNarzedziaWyslij = tk.Button(left_frame, text="Send", fg="white", bg="black", width=10,\
+        sendMsgButton = tk.Button(left_frame, text="Send", fg="white", bg="black", width=10,\
                                             relief="solid", height=2, anchor="center", \
                                             activebackground="white", activeforeground="black",\
                                             command=lambda: self.send_msg())
-        PrzyciskNarzedziaWyslij['font'] = tkFont.Font(family="Arial", size=12, weight="bold")
-        PrzyciskNarzedziaWyslij.place(x=580, y=640)
-        ############################################################################################
+        sendMsgButton['font'] = tkFont.Font(family="Arial", size=12, weight="bold")
+        sendMsgButton.place(x=580, y=640)
 
-        # szara strefa
-        ############################################################################################
 
-        textSzaraStrefa = tk.Text(right_frame, bg="#D3D3D3",yscrollcommand=1, exportselection=1, \
+        #TODO other chats will be shown here
+        field = tk.Text(right_frame, bg="#D3D3D3",yscrollcommand=1, exportselection=1, \
                                     bd=0, height=21,selectbackground="#B1B1B1", width=18, \
                                     state="disabled", highlightbackground="#D3D3D3")
-        textSzaraStrefa['font'] = tkFont.Font(family="Arial", size=20)
-        textSzaraStrefa.place(x=10, y=10)
+        field['font'] = tkFont.Font(family="Arial", size=20)
+        field.place(x=10, y=10)
 
     def send_msg(self):
         msg = self.chatEntry.get()
@@ -97,8 +107,15 @@ class GUI(object):
 
 
 if __name__ == "__main__":
+    #Check if the token is already stored in dcToken.txt
+    try:
+        with open("dcToken.txt", "r") as f:
+            DC_TOKEN = ''.join(f.readlines()) 
+    except:
+        print("Could not find a dcToken file")
     window = GUI()
     window.configure_loginPage()
+    window.configure_mainPage()
     window.showPage(window.loginPage)
     window.run()
 
