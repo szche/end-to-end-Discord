a1859bbaf6499873d50fb93d0f8e254636418520
unknown <github@chadam.pl>
2021-02-17 13:22:51 +0100

    Start Diffie-Hellman key exchange with every user on startup

--- bot.py
+++ bot.py
@@ -6,12 +6,11 @@ from aes import AESCipher
 
 database = {
         "token": None,
-        "keys": {},
+        "users": {},
         "chats": {},
 } 
 
 bot = commands.Bot(command_prefix="")
-keybase = {}
 
 def start_bot():
     global bot
@@ -27,46 +26,60 @@ async def on_ready():
     print("BOT:", database)
     users = bot.users
     for user in users:
+        print(user)
+        df = DiffieHellman()
+        k1 = df.generate_k()
         database['chats'][str(user.id)] = []
-        database['keys'][str(user.id)] = None
-    print("-" * 40)
+        database['users'][str(user.id)] = { 
+                    'name': user.name, 
+                    'key': df 
+                } 
+        print("Sending to:", user)
+        u = await bot.fetch_user(user.id)
+        try:
+            await u.send(f'===KEYEXCH===\nvalue:{k1}')
+        except:
+            print("Erorr sending key-exchange to:", user)
+        print("-" * 40)
 
 @bot.event
 async def on_message(message):
     #Ignore MSGs if they're not DMs
     #TODO implement end-to-end group chatting
-    print(database)
     if message.author == bot.user or not isinstance(message.channel, discord.DMChannel):
         return
 
-    #print(message.author)
-    #print(message.content)
     database['chats'][str(message.author.id)].append(message.content)
 
     if message.content.startswith("===KEYEXCH==="):
         # Perform a Diffie-Hellman key exchange
         print(f"Key exchange started with {message.author.name}")
-        value = message.content.find("value:")
-        k2 = int(message.content[value+6:])
-        df = DiffieHellman(k2)
+        k2 = message.content.find("value:")
+        df = DiffieHellman()
         k1 = df.generate_k()
-        key = df.generate_full_key()
-        
-        keybase[message.author.id] = key 
-
-        print("Key with user: ", message.author.name, " is ", keybase[message.author.id] )
+        key = df.generate_full_key(k2)
+        database['users'][str(message.author.id)]['key'] = df
+        print("Key with user: ", message.author.name, " is ", database['users'][str(message.author.id)]['key'].key)
         await message.author.send(f"===KEYACK===\nvalue:{k1}")
 
-    elif message.content.startswith("====KEYACK==="):
+    elif message.content.startswith("===KEYACK==="):
         value = message.content.find("value:")
-        key = message.content[value+6:]
-        keybase[message.author.id] *= int(key) 
+        k2 = message.content[value+6:]
+        df = database['users'][str(message.author.id)]['key']
+        df.generate_full_key(k2)
+        database['users'][str(message.author.id)]['key'] = df
+        print("Key with user: ", message.author.name, " is ", database['users'][str(message.author.id)]['key'].key)
 
     elif message.content.startswith("===MSG==="):
         # Recieve a message:
         content = message.content[9:]
-        aes = AESCipher( keybase[message.author.id] ) 
-        decrypted = aes.decrypt(content)
+        key = database['users'][str(message.author.id)]['key'].key
+        aes = AESCipher( key ) 
+        try:
+            decrypted = aes.decrypt(content)
+        except:
+            print("Error while decrypting message")
+            return
         print(content)
         print(decrypted)
         await message.author.send("Roger that")
