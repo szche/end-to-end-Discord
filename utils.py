05800428da74063662a7d46107318c3aa78dc84b
unknown <github@chadam.pl>
2021-02-17 13:51:52 +0100

    Hash database to be more space efficient

--- utils.py
+++ utils.py
@@ -1,5 +1,14 @@
-import pickle
+import pickle, hashlib
 
 
 def serialize(data):
     return pickle.dumps(data, protocol=4)
+
+#SHA-256 of a data
+def sha256(data):
+    return hashlib.sha256( serialize(data) ).hexdigest()
+
+
+if __name__ == "__main__":
+    test = ['abc', 'cdf', 'ebc']
+    assert sha256(test) == '22910a031de59d4f4b48b868c64cac76d1353ec5474c31be3430182f9df52aa5'
